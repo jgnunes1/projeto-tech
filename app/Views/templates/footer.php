5ab512990411e8a50ce42ebcad5e1982152eeba1
jgnunes1 <jgnunes@gmail.com>
2023-11-28 21:07:32 -0300

    tabela

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -27,7 +27,40 @@
 <script src="<?=base_url('theme/plugins/jquery/jquery.min.js')?> "></script>
 <!-- Bootstrap 4 -->
 <script src="<?=base_url('theme/plugins/bootstrap/js/bootstrap.bundle.min.js')?> "></script>
+<!-- DataTables & Plugins -->
+<script src="<?=('theme/plugins/datatables/jquery.dataTables.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-responsive/js/dataTables.responsive.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-buttons/js/dataTables.buttons.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')?>"></script>
+<script src="<?=('theme/plugins/jszip/jszip.min.js')?>"></script>
+<script src="<?=('theme/plugins/pdfmake/pdfmake.min.js')?>"></script>
+<script src="<?=('theme/plugins/pdfmake/vfs_fonts.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-buttons/js/buttons.html5.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-buttons/js/buttons.print.min.js')?>"></script>
+<script src="<?=('theme/plugins/datatables-buttons/js/buttons.colVis.min.js')?>"></script>
 <!-- AdminLTE App -->
 <script src="<?=base_url('theme/dist/js/adminlte.min.js')?> "></script>
+
+
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+
 </body>
 </html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -33,11 +33,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>UERJ-TECH</title>
-
+ 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="<?= base_url('theme/plugins/fontawesome-free/css/all.min.css') ?>">
+
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="<?= base_url('theme/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('theme/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('theme/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')?>">
+
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url('theme/dist/css/adminlte.min.css')?>">
 
